80ecfe42519b319c384f5106e86e19c421fb7dc9
C109118205 <C109118205@nkust.edu.tw>
2021-12-17 08:41:02 +0800

    add sidebar

--- huangpuGo/mainsite/models.py
+++ huangpuGo/mainsite/models.py
@@ -7,6 +7,10 @@ class Store(models.Model):
     name = models.CharField(max_length=100)
     lat = models.DecimalField(max_digits=9, decimal_places=6)
     lon = models.DecimalField(max_digits=9, decimal_places=6)
-    
+    note = models.TextField(null=True, blank=True)
+    FBLink = models.URLField(max_length = 200, null=True, blank=True)
+    IGLink = models.URLField(max_length = 200, null=True, blank=True)
+    OtherLink = models.URLField(max_length = 200, null=True, blank=True)
+
     def __str__(self):
         return self.type
